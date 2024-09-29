8672581a93fa772cd6360d9460b1f470283382fa
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2024-09-29 13:15:42 -0300

    Update Among Us download create Cs 1.6 option

--- Prompt.py
+++ Prompt.py
@@ -6,14 +6,37 @@ from os import getcwd, getenv
 from time import sleep
 from os import path
 import shutil
+import win32com.client
+
+#  Create Shortcut
+
+def create_file_shortcut(file_path, shortcut_name=None):
+    # Initialize the Windows Shell
+    shell = win32com.client.Dispatch("WScript.Shell")
+    # Get the path to the desktop
+    desktop = os.path.join(os.environ["USERPROFILE"], "Desktop")
+
+    # Set the name of the shortcut and the full path where it will be created
+    if not shortcut_name:
+        shortcut_name = os.path.basename(file_path)  # Use the original file name if no name is provided
+    shortcut_path = os.path.join(desktop, shortcut_name + ".lnk")
+
+    shortcut = shell.CreateShortcut(shortcut_path)
+
+    shortcut.TargetPath = file_path
+
+    shortcut.WorkingDirectory = os.path.dirname(file_path)
+
+    shortcut.save()
+
 
 #  Download Function
 def download(file):
     # Games url
     minecraft = ['Minecraft',"https://tlauncher.org/installer"]
     roblox = ['Roblox',"https://www.roblox.com/pt/download/client?os=win"]
-    among = ['Among Us', r"downloads\Among Us"]
-
+    among = ['Among Us',"https://download1478.mediafire.com/r8iwm6uij49gD-5rw1xygwaEQMDIx082wJqvwR22VajYQTfNr-JDegmR_kzR0O-FPgWQUWT5u4_jOUoOtrGLVWmNFEvJQGWsaLpWiHdv3vG3IjbuyZS6qfJ2F4dDeR1GJdhLXbxcEX-bYUNw3ka7Z_8woS3gJMH548Wh3dQqHWtplN8/t9i81p300f7ay5c/Among+Us.zip" ,r"downloads\Among Us"]
+    cs = ['Counter Strike 1.6',"https://download948.mediafire.com/us3zvebxtzpgBmgv-l0gSEzs7NYRXvJ2KWpEyqhFexD0nAU3rPBkSQhOd-tx8TvKWc5BRMSXdvwyRqWDZb4_dLXoRk87IyKY_X5Ks2ojlN1UUD5SXUbIXJ3xkc_7yw4lwk6Dr6lVuNYh0rr_EyE6a5XCNdHZOKVpNsYDIjG7rP5QCks/zs4ns5e52llytbj/Cs1.6.zip", r"downloads\cs1.6"]
 
     # Apps url
     pycharm = ['PyCharm',"https://download.jetbrains.com/python/pycharm-community-2024.2.0.1.exe"]
@@ -32,10 +55,24 @@ def download(file):
         print(file.capitalize())
 
     elif file == 'among':
+        file_name = path.join("downloads", file + ".zip")
+        cmd(f'powershell Invoke-WebRequest "{among[1]}" -OutFile "{among[0] + '.zip'}"')
+        shutil.unpack_archive(among[0] + '.zip', among[2], 'zip')
         appdata = getenv('APPDATA')
-        #shutil.move(among[1], f"{appdata + r'\Among Us'}")
-        shutil.copytree(among[1],f"{appdata + r'\Among Us'}" )
+        shutil.copytree(among[2],f"{appdata + r'\Among Us'}" )
         file = appdata + r"\Among Us\Among Us.exe"
+        create_file_shortcut(file, f"{among[0]}")
+        os.startfile(file)
+        print(file.capitalize())
+
+    elif file == 'cs':
+        file_name = path.join("downloads", file + ".zip")
+        cmd(f'powershell Invoke-WebRequest "{cs[1]}" -OutFile "{cs[0] + '.zip'}"')
+        shutil.unpack_archive(cs[0] + '.zip', cs[2], 'zip')
+        appdata = getenv('APPDATA')
+        shutil.copytree(cs[2], f"{appdata + r'\cs1.6'}")
+        file = appdata + r"\cs1.6\Counter-Strike WaRzOnE\CS16Launcher.exe"
+        create_file_shortcut(file, f"{cs[0]}")
         os.startfile(file)
         print(file.capitalize())
 
@@ -60,7 +97,7 @@ cmd("cls")
 # Text Samples
 
 user_text = f"{color.fg.green}({color.fg.blue}{user}@{hostname}{color.reset}{color.fg.green})-[{color.reset}{color.type.Black}{local_path}{color.fg.green}]{color.reset}"
-games_options = f"{color.fg.orange}Options:{color.reset}\n\n{color.type.Black}>{color.fg.green} 0:Minecraft{color.reset}\n{color.type.Black}>{color.fg.lightgrey} 1:Roblox{color.reset}\n{color.type.Black}>{color.fg.lightcyan} 2:Among Us{color.reset}"
+games_options = f"{color.fg.orange}Options:{color.reset}\n\n{color.type.Black}>{color.fg.green} 0:Minecraft{color.reset}\n{color.type.Black}>{color.fg.lightgrey} 1:Roblox{color.reset}\n{color.type.Black}>{color.fg.lightcyan} 2:Among Us{color.reset}\n{color.type.Black}>{color.fg.lightblue} 3:Counter Strike 1.6{color.reset}"
 app_options = f"{color.fg.orange}Options:{color.reset}\n\n{color.type.Black}>{color.fg.green} 0:PyCharm{color.reset}"
 shell_simbol = f"{color.type.Black}{color.fg.blue}\n$ {color.reset}"
 wrong_value = f"{color.fg.red}{color.type.Black}%ERROR%{color.reset} WRONG VALUE"
@@ -81,6 +118,8 @@ if option == '0':
         download("roblox")
     elif option == '2':
         download("among")
+    elif option == '3':
+        download("cs")
     else:
         print(f"{wrong_value}")
         sleep(10)
