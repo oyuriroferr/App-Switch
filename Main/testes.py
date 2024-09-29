a0f9d34a10a1cd64fedc13af8704d859547299e0
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2024-09-29 19:22:45 -0300

    Creating Installation branch

--- Main/testes.py
+++
@@ -1,3 +0,0 @@
-among = ['Among Us',"https://download1478.mediafire.com/r8iwm6uij49gD-5rw1xygwaEQMDIx082wJqvwR22VajYQTfNr-JDegmR_kzR0O-FPgWQUWT5u4_jOUoOtrGLVWmNFEvJQGWsaLpWiHdv3vG3IjbuyZS6qfJ2F4dDeR1GJdhLXbxcEX-bYUNw3ka7Z_8woS3gJMH548Wh3dQqHWtplN8/t9i81p300f7ay5c/Among+Us.zip", r"downloads\Among Us"]
-
-print(among[0].replace(" ","") + '.zip')
