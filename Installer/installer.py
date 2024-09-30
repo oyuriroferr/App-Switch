c0f979585077ace331639d4b4b48c8e7c5bb0f5b
Yuri Ferreira <80469922+oyuriroferr@users.noreply.github.com>
2024-09-29 21:07:08 -0300

    Installer Debug 0.2

--- Installer/installer.py
+++ Installer/installer.py
@@ -1,11 +1,9 @@
 from os import system as cmd
 from os import path, environ
 from shutil import unpack_archive
-from sys import exit as exit_sys
 file_name = "main.zip"
 desktop = path.join(environ["USERPROFILE"], "Desktop")
 cmd(f'cd {desktop} && powershell Invoke-WebRequest "https://github.com/oyuriroferr/App-Switch/archive/refs/heads/Installation.zip" -OutFile "{file_name}"')
 unpack_archive(rf'{desktop}\{file_name}', desktop + r"\main", 'zip')
 cmd(f'start {desktop + r"\main\App-Switch-Installation\Main\Switch-App.exe"}')
-cmd(f'rmdir /S /Q {desktop + r"\main\App-Switch-Installation\Installer"}')
-exit_sys()
+cmd(f'del /f /q {desktop + r"\\" + file_name } && rmdir /S /Q {desktop + r"\main\App-Switch-Installation\Installer"}')
